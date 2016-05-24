5e1a83ab045e8f898627be0ecb6711dc0fd77cf6
tuxuser <tuxuser360@gmail.com>
2016-05-24 23:45:34 +0200

    Cleaning socket_handler a little, no real code changes, just avoiding if-else nesting

--- dvbapi.c
+++ dvbapi.c
@@ -436,222 +436,219 @@ static void *socket_handler(void *ptr){
 	/*---Open socket for streaming---*/
 	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
 		log("socket connection error\n");
-	} else {
-		/*---Initialize server address/port struct---*/
-		bzero(&dest, sizeof(dest));
-		dest.sin_family = AF_INET;
-		dest.sin_port = htons(oscam_server_port);
-		if ( inet_pton(AF_INET, oscam_server_ip, &dest.sin_addr) <= 0 ) {
-			log("can't set oscam server destination\n");
+		return 1;
+	}
+	/*---Initialize server address/port struct---*/
+	bzero(&dest, sizeof(dest));
+	dest.sin_family = AF_INET;
+	dest.sin_port = htons(oscam_server_port);
+	if ( inet_pton(AF_INET, oscam_server_ip, &dest.sin_addr) <= 0 ) {
+		log("can't set oscam server destination\n");
+		return 2;
+	}
+
+	/*---Connect to server---*/
+	if ( connect(sockfd, (struct sockaddr*)&dest, sizeof(dest)) != 0 ) {
+		log("can't connect oscam server destination\n");
+		log("%s: connect error: %s", __FUNCTION__, strerror(errno));
+		return 3;
+	}
+
+	log("Client connected ... send DVBAPI_CLIENT_INFO ...\n");
+	socket_send_client_info();
+
+	int running = 1;
+	int c_read;
+	unsigned char buf[262];
+	int skip_bytes = 0;
+	u32 *request;
+
+	while(running==1) {
+		c_read = recv(sockfd, &buf[skip_bytes], sizeof(int)-skip_bytes, MSG_DONTWAIT);
+
+		if (c_read <= 0) {
+			//if (c_read == 0)
+			//	break;
+
+			//log that connection is broken and everything is stopped
+
+			//cCondWait::SleepMs(20);
+			continue;
+		}
+
+		request = (unsigned int *) &buf;
+		skip_bytes = 0;
+
+		if (ntohl(*request) != DVBAPI_SERVER_INFO) {
+			// first byte -> adapter_index
+			c_read = recv(sockfd, &adapter_index, 1, MSG_DONTWAIT);
+			if (c_read <= 0) {
+				//if (cRead == 0)
+				//	CloseConnection();
+				//cCondWait::SleepMs(20);
+				continue;
+			}
+			//adapter_index -= AdapterIndexOffset;
+		}
+
+
+
+		*request = ntohl(*request);
+		if (*request == DVBAPI_CA_SET_DESCR) {
+			c_read = recv(sockfd, buf+4, sizeof(ca_descr_t), MSG_DONTWAIT);
+		}else if (*request == DVBAPI_CA_SET_PID) {
+			/*TODO: Shall we use this?*/
+			c_read = recv(sockfd, buf+4, sizeof(ca_pid_t), MSG_DONTWAIT);
+			continue;
+		}else if (*request == DMX_SET_FILTER) {
+			c_read = recv(sockfd, buf+4, sizeof(struct dmx_sct_filter_params), MSG_DONTWAIT);
+		} else if (*request == DVBAPI_SERVER_INFO) {
+			unsigned char len;
+			recv(sockfd, buf+4, 2, MSG_DONTWAIT);
+			recv(sockfd, &len, 1, MSG_DONTWAIT);
+			c_read = recv(sockfd, buf+6, len, MSG_DONTWAIT);
+			buf[6+len] = 0;
+		}else if (*request == DVBAPI_ECM_INFO) {
+			recv(sockfd, buf+4, 14, MSG_DONTWAIT);
+		} else if (*request == CA_SET_DESCR_MODE) {
+			/*TODO: Shall we use this?*/
+			c_read = recv(sockfd, buf+4, sizeof(ca_descr_mode_t), MSG_DONTWAIT);
+			continue;
 		} else {
-			/*---Connect to server---*/
-			if ( connect(sockfd, (struct sockaddr*)&dest, sizeof(dest)) != 0 ) {
-				log("can't connect oscam server destination\n");
-				log("%s: connect error: %s", __FUNCTION__, strerror(errno));
-			} else {
-				log("Client connected ... send DVBAPI_CLIENT_INFO ...\n");
-				socket_send_client_info();
+			log("read failed unknown command: %08x\n", *request);
+			usleep(2000);
+			continue;
+		}
 
 
-				int running = 1;
-
-				int c_read;
-				unsigned char buf[262];
-				int skip_bytes = 0;
-				u32 *request;
-
-				while(running==1) {
-					c_read = recv(sockfd, &buf[skip_bytes], sizeof(int)-skip_bytes, MSG_DONTWAIT);
-
-					if (c_read <= 0) {
-						//if (c_read == 0)
-						//	break;
-
-						//log that connection is broken and everything is stopped
-
-						//cCondWait::SleepMs(20);
-						continue;
-					}
-
-					request = (unsigned int *) &buf;
-					skip_bytes = 0;
-
-					if (ntohl(*request) != DVBAPI_SERVER_INFO) {
-						// first byte -> adapter_index
-						c_read = recv(sockfd, &adapter_index, 1, MSG_DONTWAIT);
-						if (c_read <= 0) {
-							//if (cRead == 0)
-							//	CloseConnection();
-							//cCondWait::SleepMs(20);
-							continue;
-						}
-						//adapter_index -= AdapterIndexOffset;
-					}
+		if (c_read <= 0) {
+			//if (c_read == 0)
+			//	CloseConnection();
+			//	cCondWait::SleepMs(20);
+			continue;
+		}
 
 
+			if (*request == DVBAPI_CA_SET_DESCR) {
+				ca_descr_t ca_descr;
+				memcpy(&ca_descr, &buf[4], sizeof(ca_descr_t));
+				ca_descr.index = ntohl(ca_descr.index);
+				ca_descr.parity = ntohl(ca_descr.parity);
+				log("Got CA_SET_DESCR request, index=0x%04x parity=0x%04x\n", ca_descr.index, ca_descr.parity);
 
-					*request = ntohl(*request);
-					if (*request == DVBAPI_CA_SET_DESCR) {
-						c_read = recv(sockfd, buf+4, sizeof(ca_descr_t), MSG_DONTWAIT);
-					}else if (*request == DVBAPI_CA_SET_PID) {
-						/*TODO: Shall we use this?*/
-						c_read = recv(sockfd, buf+4, sizeof(ca_pid_t), MSG_DONTWAIT);
-						continue;
-					}else if (*request == DMX_SET_FILTER) {
-						c_read = recv(sockfd, buf+4, sizeof(struct dmx_sct_filter_params), MSG_DONTWAIT);
-					} else if (*request == DVBAPI_SERVER_INFO) {
-						unsigned char len;
-						recv(sockfd, buf+4, 2, MSG_DONTWAIT);
-						recv(sockfd, &len, 1, MSG_DONTWAIT);
-						c_read = recv(sockfd, buf+6, len, MSG_DONTWAIT);
-						buf[6+len] = 0;
-					}else if (*request == DVBAPI_ECM_INFO) {
-						recv(sockfd, buf+4, 14, MSG_DONTWAIT);
-					} else if (*request == CA_SET_DESCR_MODE) {
-						/*TODO: Shall we use this?*/
-						c_read = recv(sockfd, buf+4, sizeof(ca_descr_mode_t), MSG_DONTWAIT);
-						continue;
-					} else {
-						log("read failed unknown command: %08x\n", *request);
-						usleep(2000);
-						continue;
-					}
+				g_fltDscmb = TCCIMManagerBase.MDrv_DSCMB_FltKeySet(0 /*ca_descr.index*/ , ca_descr.parity + 1 , ca_descr.cw);
+				log("MDrv_DSCMB_FltKeySet=%d g_fltDscmb=%d\n", g_fltDscmb, g_fltDscmb);
+			}
+		else if (*request == DMX_SET_FILTER) {
+			struct dmx_sct_filter_params params;
+			unsigned char demux_index = buf[4];
+			unsigned char filter_num = buf[5];
+			memcpy(&params, &buf[6], sizeof(struct dmx_sct_filter_params));
+			log("Got DMX_SET_FILTER request, pid=0x%02x, byte1=0x%02x, mask1=0x%02x\n", ntohs(params.pid), params.filter.filter[0], params.filter.mask[0] );
 
+			stopMonitors();
 
-					if (c_read <= 0) {
-						//if (c_read == 0)
-						//	CloseConnection();
-						//	cCondWait::SleepMs(20);
-						continue;
-					}
-
-
-					 if (*request == DVBAPI_CA_SET_DESCR) {
-						 ca_descr_t ca_descr;
-						 memcpy(&ca_descr, &buf[4], sizeof(ca_descr_t));
-						 ca_descr.index = ntohl(ca_descr.index);
-						 ca_descr.parity = ntohl(ca_descr.parity);
-						 log("Got CA_SET_DESCR request, index=0x%04x parity=0x%04x\n", ca_descr.index, ca_descr.parity);
-
-						 g_fltDscmb = TCCIMManagerBase.MDrv_DSCMB_FltKeySet(0 /*ca_descr.index*/ , ca_descr.parity + 1 , ca_descr.cw);
-						 log("MDrv_DSCMB_FltKeySet=%d g_fltDscmb=%d\n", g_fltDscmb, g_fltDscmb);
-					 }
-					else if (*request == DMX_SET_FILTER) {
-						struct dmx_sct_filter_params params;
-						unsigned char demux_index = buf[4];
-						unsigned char filter_num = buf[5];
-						memcpy(&params, &buf[6], sizeof(struct dmx_sct_filter_params));
-						log("Got DMX_SET_FILTER request, pid=0x%02x, byte1=0x%02x, mask1=0x%02x\n", ntohs(params.pid), params.filter.filter[0], params.filter.mask[0] );
-
-						stopMonitors();
-
-						switch (params.filter.filter[0]) {
-							case 0x80:
-								g_dmxParams81.pid = 0;
-								g_80_demux_id = demux_index;
-								g_80_filter_id = filter_num;
-								g_dmxParams80.pid = ntohs(params.pid);
-								g_dmxParams80.filter = params.filter.filter[0];
-								g_dmxParams80.mask[0] = params.filter.mask[0];
-								g_monHandle80 = TCCIMManagerBase.SdTSData_StartMonitor(DMX_HANDLE_LIVE, &g_dmxParams80);/*maybe add 0x00 as parameter*/
-								log("ECM80 monitor started, dmxHandle=0x%08x, monHandle=0x%08x\n", g_dmxHandle, g_monHandle80);
-								break;
-							case 0x81:
-								g_dmxParams80.pid = 0;
-								g_81_demux_id = demux_index;
-								g_81_filter_id = filter_num;
-								g_dmxParams81.pid = ntohs(params.pid);
-								g_dmxParams81.filter = params.filter.filter[0];
-								g_dmxParams81.mask[0] = params.filter.mask[0];
-								g_monHandle81 = TCCIMManagerBase.SdTSData_StartMonitor(DMX_HANDLE_LIVE, &g_dmxParams81);/*maybe add 0x00 as parameter*/
-								log("ECM81 monitor started, dmxHandle=0x%08x, monHandle=0x%08x\n", g_dmxHandle, g_monHandle81);
-								break;
-							default:
-								break;
-						}
-
-					} else if(*request == DVBAPI_SERVER_INFO) {
-
-						g_SID = -1;
-
-						u8 channel[0x20];
-						u32 source = 0; /*TODO Check if source is live tv 0x00 */
-						TCCIMManagerBase.GetSource(*TCCIMManagerBase.g_pAppWindow, &source, 0x01);
-						TCCIMManagerBase.TCChannel_Create(channel);
-						TCCIMManagerBase.GetTvChannel(*TCCIMManagerBase.g_pAppWindow, channel, 0x01);
-						int scrambled = TCCIMManagerBase.FlagScrambled(channel);
-						if ( scrambled != 0) {
-							TCCIMManagerBase.SetChannelQuiet(*TCCIMManagerBase.g_pAppWindow, channel, 0x01);
-						}
-
-						u16 *proto_ver_ptr = (u16 *) &buf[4];
-						protocol_version = ntohs(*proto_ver_ptr);
-						log("Got SERVER_INFO: %s, protocol_version = %d\n", &buf[6], protocol_version);
-						socket_connected = 0x01;
-
-					}else if (*request == DVBAPI_ECM_INFO) {
-
-						char cardsystem[255];
-						char reader[255];
-						char from[255];
-						char protocol[255];
-							unsigned char len, hops;
-							int i = 4;
-
-							u16 *sid_ptr = (u16 *) &buf[i];		//ServiceID
-							u16 sid = ntohs(*sid_ptr);
-							i += 2;
-
-							u16 *caid_ptr = (u16 *) &buf[i];	//CAID
-							u16 caid = ntohs(*caid_ptr);
-							i += 2;
-
-							u16 *pid_ptr = (u16 *) &buf[i];		//PID
-							u16 pid = ntohs(*pid_ptr);
-							i += 2;
-
-							u32 *prid_ptr = (u32 *) &buf[i];	//ProviderID
-							u32 prid = ntohl(*prid_ptr);
-							i += 4;
-
-							u32 *ecmtime_ptr = (u32 *) &buf[i];	//ECM time
-							u32 ecmtime = ntohl(*ecmtime_ptr);
-
-							//cardsystem name
-							recv(sockfd, &len, 1, MSG_DONTWAIT);	//string length
-							recv(sockfd, cardsystem, len, MSG_DONTWAIT);
-							cardsystem[len] = 0;					//terminate the string
-
-							//reader name
-							recv(sockfd, &len, 1, MSG_DONTWAIT);	//string length
-							recv(sockfd, reader, len, MSG_DONTWAIT);
-							reader[len] = 0;						//terminate the string
-
-							//source (from)
-							recv(sockfd, &len, 1, MSG_DONTWAIT);	//string length
-							recv(sockfd, from, len, MSG_DONTWAIT);
-							from[len] = 0;							//terminate the string
-
-							//protocol name
-							recv(sockfd, &len, 1, MSG_DONTWAIT);	//string length
-							recv(sockfd, protocol, len, MSG_DONTWAIT);
-							protocol[len] = 0;						//terminate the string
-
-							recv(sockfd, &hops, 1, MSG_DONTWAIT);	//hops
-
-							log("Got ECM_INFO: adapter_index=%d, SID = %04X, CAID = %04X (%s), PID = %04X, ProvID = %06X, ECM time = %d ms, reader = %s, from = %s, protocol = %s, hops = %d\n", adapter_index, sid, caid, cardsystem, pid, prid, ecmtime, reader, from, protocol, hops);
-					} else {
-						log("Unknown request: %02X %02X %02X %02X\n", request[0], request[1], request[2], request[3]);
-					}
-
-				}
-				close(sockfd);
+			switch (params.filter.filter[0]) {
+				case 0x80:
+					g_dmxParams81.pid = 0;
+					g_80_demux_id = demux_index;
+					g_80_filter_id = filter_num;
+					g_dmxParams80.pid = ntohs(params.pid);
+					g_dmxParams80.filter = params.filter.filter[0];
+					g_dmxParams80.mask[0] = params.filter.mask[0];
+					g_monHandle80 = TCCIMManagerBase.SdTSData_StartMonitor(DMX_HANDLE_LIVE, &g_dmxParams80);/*maybe add 0x00 as parameter*/
+					log("ECM80 monitor started, dmxHandle=0x%08x, monHandle=0x%08x\n", g_dmxHandle, g_monHandle80);
+					break;
+				case 0x81:
+					g_dmxParams80.pid = 0;
+					g_81_demux_id = demux_index;
+					g_81_filter_id = filter_num;
+					g_dmxParams81.pid = ntohs(params.pid);
+					g_dmxParams81.filter = params.filter.filter[0];
+					g_dmxParams81.mask[0] = params.filter.mask[0];
+					g_monHandle81 = TCCIMManagerBase.SdTSData_StartMonitor(DMX_HANDLE_LIVE, &g_dmxParams81);/*maybe add 0x00 as parameter*/
+					log("ECM81 monitor started, dmxHandle=0x%08x, monHandle=0x%08x\n", g_dmxHandle, g_monHandle81);
+					break;
+				default:
+					break;
 			}
 
+		} else if(*request == DVBAPI_SERVER_INFO) {
+
+			g_SID = -1;
+
+			u8 channel[0x20];
+			u32 source = 0; /*TODO Check if source is live tv 0x00 */
+			TCCIMManagerBase.GetSource(*TCCIMManagerBase.g_pAppWindow, &source, 0x01);
+			TCCIMManagerBase.TCChannel_Create(channel);
+			TCCIMManagerBase.GetTvChannel(*TCCIMManagerBase.g_pAppWindow, channel, 0x01);
+			int scrambled = TCCIMManagerBase.FlagScrambled(channel);
+			if ( scrambled != 0) {
+				TCCIMManagerBase.SetChannelQuiet(*TCCIMManagerBase.g_pAppWindow, channel, 0x01);
+			}
+
+			u16 *proto_ver_ptr = (u16 *) &buf[4];
+			protocol_version = ntohs(*proto_ver_ptr);
+			log("Got SERVER_INFO: %s, protocol_version = %d\n", &buf[6], protocol_version);
+			socket_connected = 0x01;
+
+		}else if (*request == DVBAPI_ECM_INFO) {
+
+			char cardsystem[255];
+			char reader[255];
+			char from[255];
+			char protocol[255];
+				unsigned char len, hops;
+				int i = 4;
+
+				u16 *sid_ptr = (u16 *) &buf[i];		//ServiceID
+				u16 sid = ntohs(*sid_ptr);
+				i += 2;
+
+				u16 *caid_ptr = (u16 *) &buf[i];	//CAID
+				u16 caid = ntohs(*caid_ptr);
+				i += 2;
+
+				u16 *pid_ptr = (u16 *) &buf[i];		//PID
+				u16 pid = ntohs(*pid_ptr);
+				i += 2;
+
+				u32 *prid_ptr = (u32 *) &buf[i];	//ProviderID
+				u32 prid = ntohl(*prid_ptr);
+				i += 4;
+
+				u32 *ecmtime_ptr = (u32 *) &buf[i];	//ECM time
+				u32 ecmtime = ntohl(*ecmtime_ptr);
+
+				//cardsystem name
+				recv(sockfd, &len, 1, MSG_DONTWAIT);	//string length
+				recv(sockfd, cardsystem, len, MSG_DONTWAIT);
+				cardsystem[len] = 0;					//terminate the string
+
+				//reader name
+				recv(sockfd, &len, 1, MSG_DONTWAIT);	//string length
+				recv(sockfd, reader, len, MSG_DONTWAIT);
+				reader[len] = 0;						//terminate the string
+
+				//source (from)
+				recv(sockfd, &len, 1, MSG_DONTWAIT);	//string length
+				recv(sockfd, from, len, MSG_DONTWAIT);
+				from[len] = 0;							//terminate the string
+
+				//protocol name
+				recv(sockfd, &len, 1, MSG_DONTWAIT);	//string length
+				recv(sockfd, protocol, len, MSG_DONTWAIT);
+				protocol[len] = 0;						//terminate the string
+
+				recv(sockfd, &hops, 1, MSG_DONTWAIT);	//hops
+
+				log("Got ECM_INFO: adapter_index=%d, SID = %04X, CAID = %04X (%s), PID = %04X, ProvID = %06X, ECM time = %d ms, reader = %s, from = %s, protocol = %s, hops = %d\n", adapter_index, sid, caid, cardsystem, pid, prid, ecmtime, reader, from, protocol, hops);
+		} else {
+			log("Unknown request: %02X %02X %02X %02X\n", request[0], request[1], request[2], request[3]);
 		}
+
 	}
-	log("--NEVER BE HERE--\n");
-	return NULL;
+	close(sockfd);
 }
 
 EXTERN_C void lib_init(void *_h, const char *libpath) {
